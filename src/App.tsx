3a0dcdd76297c9c56136535f5604398e3136f83f
Sven <s.fehler@pbsnetwork.eu>
2022-11-03 00:00:18 +0100

    some more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import THREE, { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { Canvas } from "@react-three/fiber";
 import { Cube } from "./components/Cube";
+import { useState } from "react";
+import { Box } from "@mui/system";
 
 export function App() {
-  // const scene = new Scene();
-  // const camera = new PerspectiveCamera(
-  //   75,
-  //   window.innerWidth / window.innerHeight,
-  //   0.1,
-  //   1000
-  // );
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Cube position={[-3, 0, 0]} />
-        <Cube position={[3, 0, 0]} />
-      </Canvas>
+      {isLoading ? (
+        <Box>Loading...</Box>
+      ) : (
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Cube position={[-3, 0, 0]} />
+          <Cube position={[3, 0, 0]} />
+          <mesh>
+            <sphereGeometry />
+            <meshStandardMaterial color="red" />
+          </mesh>
+        </Canvas>
+      )}
     </>
   );
 }
