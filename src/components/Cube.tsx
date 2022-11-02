0ec027d56aae6b3dd0eab6671f9e1f2986bf5297
Sven <s.fehler@pbsnetwork.eu>
2022-11-03 00:00:50 +0100

    remove unnecessary stuff

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -3,19 +3,12 @@ import { useRef, useState } from "react";
 import { useFrame, ThreeElements } from "@react-three/fiber";
 
 export function Cube(props: ThreeElements["mesh"]) {
-  const ref = useRef<THREE.Mesh>(null!);
   const [hovering, setHovering] = useState(false);
   const [clicked, setClicked] = useState(false);
-
-  useFrame((_state, _deltaTime) => {
-    // console.log("FPS:", Math.floor(1000 / _deltaTime));
-    // console.log(ref.current);
-  });
 
   return (
     <mesh
       {...props}
-      ref={ref}
       scale={clicked ? 2 : 1}
       onClick={() => setClicked(!clicked)}
       onPointerOver={() => setHovering(true)}
