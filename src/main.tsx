52b7df6e2903ae1dcd148984e8fdf3252e35ab79
Sv443 <contact@sv443.net>
2022-11-03 13:08:20 +0100

    stuff that dont work

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
-import { WebGLRenderer } from "three";
+import { WebGLRenderer, Scene, PerspectiveCamera } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { App } from "./App";
 import { AppDecorator } from "./AppDecorator";
 import { createTheme } from "./theme";
@@ -20,4 +21,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 window.addEventListener("resize", () =>
   renderer.setSize(window.innerWidth, window.innerHeight)
 );
-renderer.domElement;
+
+// document.body.appendChild(renderer.domElement);
+
+// const scene = new Scene();
+
+// const camera = new PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   10000
+// );
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.addEventListener("change", animate);
+
+// //controls.update() must be called after any manual changes to the camera's transform
+// camera.position.set(0, 20, 100);
+// controls.update();
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   // required if controls.enableDamping or controls.autoRotate are set to true
+//   controls.update();
+
+//   renderer.render(scene, camera);
+// }
